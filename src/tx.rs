9a598572c966dafbfdf9ec45e45555e2c10c5b02
Chris Connelly <chris@connec.co.uk>
2024-07-26 21:22:16 +0100

    chore!: upgrade `sqlx`
    BREAKING CHANGE: `sqlx` 0.7 is no longer supported.

--- src/tx.rs
+++ src/tx.rs
@@ -232,10 +232,7 @@ where
         self,
         sql: &'q str,
         parameters: &'e [<Self::Database as sqlx::Database>::TypeInfo],
-    ) -> BoxFuture<
-        'e,
-        Result<<Self::Database as sqlx::database::HasStatement<'q>>::Statement, sqlx::Error>,
-    >
+    ) -> BoxFuture<'e, Result<<Self::Database as sqlx::Database>::Statement<'q>, sqlx::Error>>
     where
         'c: 'e,
     {
